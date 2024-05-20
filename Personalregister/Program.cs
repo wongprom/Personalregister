f9c765de3330ad6fecbb413d78ef5c02c1c32c43
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-20 06:05:45 -0700

    Init commit. Get user input namn and salary.

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
+            //Uppgift 1
+            //Vilka klasser bör ingå i programmet?
+            //Svar: person
+
+            //Uppgift 2
+            //Vilka attribut och metoder bör ingå i dessa klasser?
+            // attribut = namn, lön
+            // metoder = vet ej än
+
+            //Uppgift 3
+            //Skriv programmet
+
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen, kan du fylla i ditt namn: ");
+
+            string namn = Console.ReadLine();
+            Console.WriteLine(namn);
+            Console.WriteLine("och din lön: ");
+            string lon = Console.ReadLine();
+
+
+
+            Console.WriteLine(lon, namn); 
         }
     }
 }
