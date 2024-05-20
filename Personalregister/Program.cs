87490ab2ffee5c29f9f5c4cdde6dae41a234eaf9
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-20 05:39:50 -0700

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
