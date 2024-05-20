69b3a9755904ad42c18c6e2ce2aeca9a81221527
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-20 07:03:46 -0700

    Create while loop to continue or not adding new person. Add attribute Id to Person class

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -8,6 +8,7 @@ namespace Personalregister
         {
             public string Namn { get; set; } = string.Empty;
             public int Lon { get; set; }
+            public int Id { get; set; }
         }
         static void Main(string[] args)
         {
@@ -17,7 +18,7 @@ namespace Personalregister
 
             //Uppgift 2
             //Vilka attribut och metoder bör ingå i dessa klasser?
-            // attribut = namn, lön
+            // attribut = namn, lön, id
             // metoder = vet ej än
 
             //Uppgift 3
@@ -25,26 +26,33 @@ namespace Personalregister
 
             List<Person> personalregister = new List<Person>();
             bool laggaTillNyPerson = true;
+            
             while (laggaTillNyPerson)
             {
                 Console.WriteLine("Skriv ditt namn: ");
                 string namn = Console.ReadLine();
-                Console.WriteLine(namn);
                 Console.WriteLine("Skriv din lön: ");
                 string lon = Console.ReadLine();
-                int antalAnstallda = personalregister.Count;
-                Console.WriteLine(antalAnstallda);
-
+                int personId = personalregister.Count + 1;
                 Person person = new Person
                 {
+                    Id = personId,
                     Namn = namn,
                     Lon = Int32.Parse(lon)
                 };
 
                 personalregister.Add(person);
 
-                
+
+                Console.WriteLine("Vill du lägga till fler personer, skriv JA eller NEJ");
+                string skaLaggaTillFler = Console.ReadLine().ToLower();
+
+                if(skaLaggaTillFler != "ja")
+                {
+                    laggaTillNyPerson = false;
+                }
             }
+            Console.WriteLine($"Vi har {personalregister.Count}personer i vårat Personalregister.");
             
         }
     }
